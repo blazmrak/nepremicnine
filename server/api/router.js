b37135f3630cbc5fd2bcc86acf95e3d61aeb7e88
Blaž Mrak <blaz.mrak13@gmail.com>
2020-10-13 20:40:30 +0200

    Routing za upravljanje nepremičnin #13

--- server/api/router.js
+++ server/api/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authResource = require('./resources/auth.resource');
+const realestateResource = require('./resources/realestate.resource');
 
 router.use('/auth', authResource);
+router.use('/realestates', realestateResource);
 
 
 module.exports = router;
