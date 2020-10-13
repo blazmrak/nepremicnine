14ea342d209b7bbbfd73b641dd3e0f0871272a60
Blaž Mrak <blaz.mrak13@gmail.com>
2020-10-13 20:33:20 +0200

    dodal koordinate v tabelo nepremičnin #8

--- migrations/20201013114416-create_table_realestate.js
+++ migrations/20201013114416-create_table_realestate.js
@@ -7,16 +7,12 @@ module.exports.up = runSql(sql`
     (
         id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 ),
         address character varying(64) NOT NULL,
+        lat double precision NOT NULL,
+        lng double precision NOT NULL,
         picture character varying(254) NOT NULL,
         capacity integer NOT NULL,
-        id_owner integer NOT NULL,
         price double precision NOT NULL,
-        PRIMARY KEY (id),
-        FOREIGN KEY (id_owner)
-            REFERENCES client(id) MATCH SIMPLE
-            ON UPDATE NO ACTION
-            ON DELETE NO ACTION
-            NOT VALID
+        PRIMARY KEY (id)
     );
 `);
 module.exports.down = runSql(sql`
