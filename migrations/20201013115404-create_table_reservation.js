b75c95e14b9240693fbd05c7fd8ca6008130e393
Blaž Mrak <blaz.mrak13@gmail.com>
2020-10-13 13:57:02 +0200

    Dodal tabelo za rezervacije. Closes #9

---
+++ migrations/20201013115404-create_table_reservation.js
@@ -0,0 +1,29 @@
+
+const { sql } = require('slonik');
+const { runSql } = require('../migrator/migratorUtil');
+
+module.exports.up = runSql(sql`
+    CREATE TABLE IF NOT EXISTS reservation
+    (
+        id integer NOT NULL,
+        id_realestate integer NOT NULL,
+        id_client integer NOT NULL,
+        date_from date NOT NULL,
+        date_to date NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (id_client)
+            REFERENCES client (id) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID,
+        FOREIGN KEY (id_realestate)
+            REFERENCES realestate (id) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID
+    );
+`);
+module.exports.down = runSql(sql`
+    DROP TABLE IF EXISTS reservation;
+`);
+    
