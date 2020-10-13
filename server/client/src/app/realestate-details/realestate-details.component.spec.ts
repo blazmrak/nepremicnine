ebf0187af03d35e87c8943c8efff97add4fc31fc
Blaž Mrak <blaz.mrak13@gmail.com>
2020-10-13 20:35:42 +0200

    Dodal podrobnosti nepremičnine #4

---
+++ server/client/src/app/realestate-details/realestate-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RealestateDetailsComponent } from './realestate-details.component';
+
+describe('RealestateDetailsComponent', () => {
+  let component: RealestateDetailsComponent;
+  let fixture: ComponentFixture<RealestateDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RealestateDetailsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RealestateDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ server/client/src/app/realestate-details/realestate-details.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RealestatesService } from '../services/realestates.service';
+import { ReservationsService } from '../services/reservations.service';
+
+@Component({
+  selector: 'app-realestate-details',
+  templateUrl: './realestate-details.component.html',
+  styleUrls: ['./realestate-details.component.scss']
+})
+export class RealestateDetailsComponent implements OnInit {
+
+  public realestate: any;
+  public reservationForm: FormGroup = this.fb.group({
+    from: [null, Validators.required],
+    to: [null, Validators.required]
+  });
+
+  constructor(
+    private realestateService: RealestatesService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private reservationService: ReservationsService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.realestateService
+      .getRealestateDetails(this.route.snapshot.params.id)
+      .then(res => {
+        this.realestate = res;
+      });
+  }
+
+  public makeReservation(): void {
+    if (this.reservationForm.valid) {
+      const reservation = {
+        realestate_id: this.realestate.id,
+        from: this.reservationForm.value.from,
+        to: this.reservationForm.value.to,
+      };
+
+      this.reservationService.makeReservation(reservation).then(res => {
+        this.router.navigate(['/']);
+      });
+    }
+  }
+}
