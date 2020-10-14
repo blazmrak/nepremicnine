763a40d4dd8a30f87db91ca21e856d4487239a17
Blaž Mrak <blaz.mrak13@gmail.com>
2020-10-14 15:26:26 +0200

    added port in env

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 (async () => {
     const express = require('express');
     const app = express();
-    const port = 3000;
     const bodyParser = require('body-parser')
     const cookieParser = require('cookie-parser')
     const path = require('path')
 
     require('dotenv').config();
+
+    const port = process.env.PORT || 3000;
 
     require('db-migrate').getInstance(true).up();
 
