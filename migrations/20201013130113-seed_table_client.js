6b31d90ee0e6efd68adf7b4404fee86eb70afdd9
Blaž Mrak <blaz.mrak13@gmail.com>
2020-10-14 15:36:35 +0200

    spremenil geslo za test

--- migrations/20201013130113-seed_table_client.js
+++ migrations/20201013130113-seed_table_client.js
@@ -4,7 +4,7 @@ const { runSql } = require('../migrator/migratorUtil');
 
 module.exports.up = runSql(sql`
     INSERT INTO client(username, password, role) VALUES ('admin', '$2b$10$gt1W7Pp5k38Rspo.YVl7HOycsLnqpMAPPQuTP2TefVau93lRHRdLC', 'admin');
-    INSERT INTO client(username, password, role) VALUES ('test', '$2b$10$AmXj9Q78eFlCmXQCkET6/.Hg9WR/ru5fIE6mp8bbWFu4FWZx9UUP6', 'user');
+    INSERT INTO client(username, password, role) VALUES ('test', '$2b$10$DqjfqFY9de8nS57hGMZ4bOK7uxRMD0hxwmef.lOQ9DBBo8S3QvBjW', 'user');
 `);
 module.exports.down = runSql(sql`
     DELETE FROM client WHERE username IN ('test', 'admin');
